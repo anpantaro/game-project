8e0df82211464473ca4b02bad78d543044308315
anpantaro <anpanmk2@gmail.com>
2018-11-02 14:46:03 +0900

    gimikku no test

---
+++ Assets/Scripts/GimmickButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GodTouches;
+
+public class GimmickButton : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Button(string gimmick)
+    {
+        switch (gimmick)
+        {
+            case "Up":
+                Installation.gimmick = Gimmick.Up;
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Installation.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GodTouches;
+
+public class Installation : MonoBehaviour {
+
+    public Camera camera;
+    [SerializeField] Material[] matArray;
+    public static Gimmick gimmick;
+    public string tag;
+    public Material mat;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (gimmick != Gimmick.None)
+        {
+            tag = gimmick.ToString();
+            mat = matArray[(int)gimmick];
+            gimmick = Gimmick.None;
+        }
+        TouchJudg();
+
+    }
+
+    public void TouchJudg()
+    {
+        var phase = GodTouch.GetPhase();
+        var pos = GodTouch.GetPosition();
+
+        switch (phase)
+        {
+            case GodPhase.Began: // 押された
+                RaycastHit hit;
+                Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out hit))
+                {
+
+                    // レイに当たったオブジェクトに何かをする
+                    hit.transform.tag = tag;
+                    hit.collider.GetComponent<Renderer>().material = mat;
+
+                }
+                break;
+        }
+    }
+
+}
+public enum Gimmick
+{
+    Right,
+    Left,
+    Up,
+    Down,
+    None
+}
